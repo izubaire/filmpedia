929fa00fa4fdd6c77f42e83fd0ad37449b5a04bb
izubaire <m.zobaire@gmail.com>
2022-06-23 16:09:47 +0500

    make navbar, sidebar and adding redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 
-import useStyles from '../style';
+import useStyles from '../styles';
 
 import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
 
