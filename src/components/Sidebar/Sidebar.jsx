941ca933e5fd1172d6259934061b9c7d4b02c586
izubaire <m.zobaire@gmail.com>
2022-06-27 10:15:52 +0500

    rendering movies as well as rendering genres and categorieson sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,9 @@ import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/material/styles';
 
+import { useGetGenresQuery } from '../../services/TMDB';
 import useStyles from './styles';
+import genreIcons from '../../assets/genres';
 
 const categories = [
   { label: 'Popular', value: 'popular' },
@@ -11,16 +13,13 @@ const categories = [
   { label: 'Upcoming', value: 'upcoming' }
 ];
 
-const demoCategories = [
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Action', value: 'action' },
-  { label: 'Horror', value: 'horror' },
-  { label: 'Animation', value: 'animation' },
-];
 
 const Sidebar = ({ setMobileOpen }) => {
   const theme = useTheme();  
   const classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
+
+  console.log(data);
 
   const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
   const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
@@ -40,9 +39,9 @@ const Sidebar = ({ setMobileOpen }) => {
           {categories.map(({ label, value}) => (
             <Link key={value} className={classes.links} to="/" >
               <ListItem onClick={() => {}} button>
-                {/* <ListItemIcon>
-                  <img src={redLogo} className={classes.genreImages} height={30} />
-                </ListItemIcon> */}
+                <ListItemIcon>
+                  <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30} />
+                </ListItemIcon>
                 <ListItemText primary={label} />
               </ListItem>
             </Link>
@@ -51,13 +50,18 @@ const Sidebar = ({ setMobileOpen }) => {
         <Divider/>
         <List>
           <ListSubheader>Genres</ListSubheader>
-          {demoCategories.map(({ label, value}) => (
-            <Link key={value} className={classes.links} to="/" >
+
+          {isFetching ? (
+            <Box display="flex" justifyContent="center" >
+              <CircularProgress/>
+            </Box>
+          ) : data.genres.map(({ name, id}) => (
+            <Link key={name} className={classes.links} to="/" >
               <ListItem onClick={() => {}} button>
-                {/* <ListItemIcon>
-                  <img src={redLogo} className={classes.genreImages} height={30} />
-                </ListItemIcon> */}
-                <ListItemText primary={label} />
+                <ListItemIcon>
+                  <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30} />
+                </ListItemIcon>
+                <ListItemText primary={name} />
               </ListItem>
             </Link>
           ))}
